239136c96b8ca5d31a05f64b27d5c6860ba9d1ee
oAngulo-sh <andrewsmoehlecke@gmail.com>
2021-05-25 22:36:08 -0300

    first commit

---
+++ src/test/java/com/api/sportsmanager/SportsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sportsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
